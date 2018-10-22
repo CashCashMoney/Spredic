56ba2059d92d946d4468e82675abdd60fc634a39
Robert Bolt <robertbolt@Roberts-MacBook-Air.local>
2018-10-22 18:31:13 -0500

    initial changes

--- Spredic/AppDelegate.swift
+++ Spredic/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "myAppId"
+            configuration.server = "http://spredic.herokuapp.com/dating"
+            configuration.clientKey = "23rhir23n23ufifn23r9o23r823r"
+        }))
         return true
     }
 
